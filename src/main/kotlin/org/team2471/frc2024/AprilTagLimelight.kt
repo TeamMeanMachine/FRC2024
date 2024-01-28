2c5bf84120349b52ba2e134a5b414e6f051b6ea0
TMMShared <camasinfiniteresource@gmail.com>
2024-01-28 15:28:35 -0800

    ask peter

--- src/main/kotlin/org/team2471/frc2024/AprilTagLimelight.kt
+++ src/main/kotlin/org/team2471/frc2024/AprilTagLimelight.kt
@@ -1,7 +1,44 @@
 package org.team2471.frc2024
 
+import edu.wpi.first.networktables.NetworkTable
+import edu.wpi.first.networktables.NetworkTableInstance
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.team2471.frc.lib.coroutines.MeanlibDispatcher
+import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.Subsystem
+import org.team2471.frc.lib.math.Vector2
 
 object AprilTagLimelight : Subsystem("ApriltagLimelight") {
+    val limelightNetworkTable : NetworkTable = NetworkTableInstance.getDefault().getTable("limelight-shoot")
+    val nt : NetworkTable = NetworkTableInstance.getDefault().getTable("Shoot Limelight")
+    val totalLatencyEntry = nt.getEntry("Total Latency")
 
+    init {
+
+        GlobalScope.launch(MeanlibDispatcher) {
+
+            periodic {
+                if (seesApriltags()) {
+                    val botposeData : DoubleArray = limelightNetworkTable.getEntry("botpose_wpired").getDoubleArray(DoubleArray(0))
+                    print(botposeData)
+                    val totalLatency : Double = botposeData[16]
+                    totalLatencyEntry.setDouble(totalLatency)
+                    val fieldPose : Vector2 = wpiRedToMMCoords(Vector2(botposeData[3], botposeData[7]))
+                }
+            }
+        }
+    }
+
+    override suspend fun default() {
+
+    }
+
+    fun seesApriltags(): Boolean {
+        return limelightNetworkTable.getEntry("tv").getBoolean(false)
+    }
+
+    private fun wpiRedToMMCoords(position : Vector2) : Vector2 {
+        return Vector2(-(position.y - 13.46875), position.x - 27.1354)
+    }
 }
--- src/main/kotlin/org/team2471/frc2024/NoteLimelight.kt
+++ src/main/kotlin/org/team2471/frc2024/NoteLimelight.kt
@@ -5,44 +5,58 @@ import edu.wpi.first.networktables.NetworkTableInstance
 import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.framework.Subsystem
 import org.team2471.frc.lib.math.Vector2
+import java.util.Vector
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.math.tan
 
 object NoteLimelight : Subsystem("NoteLimelight") {
     val limelightNetworkTable: NetworkTable = NetworkTableInstance.getDefault().getTable("limelight-intake")
+    val nt : NetworkTable = NetworkTableInstance.getDefault().getTable("Intake Limelight")
+    val totalLatencyEntry = nt.getEntry("Total Latency")
     val limelightOffset = Vector2(0.0, 1.0)
     val noteDistFromCenter = 6.0
-    val limelightHeight = 9.0
+    val limelightHeight = 9.0 // change when LL put on robot; height to camera
     val limelightVerticalFOV = 49.7
     val limelightScreenHeight = 320.0
-    init {
+    val limelightScreenWidth = 320.0
 
-    }
+    var notes : ArrayList<Vector2> = arrayListOf()
 
     override suspend fun default() {
         periodic {
+
+            val totalLatency = limelightNetworkTable.getEntry("tl").getDouble(0.0) + limelightNetworkTable.getEntry("cl").getDouble(0.0)
+            totalLatencyEntry.setDouble(totalLatency);
+
             if (seesNotes()){
+                notes = ArrayList()
                 for (entryNum in 0..7){
                     if (limelightNetworkTable.getEntry("ta${entryNum}").getDouble(0.0) == 0.0){
+                        val xOffset : Double = limelightNetworkTable.getEntry("tx${entryNum}").getDouble(0.0)
+                        val boundingBoxY = limelightNetworkTable.getEntry("ty${entryNum}").getDouble(0.0)/limelightVerticalFOV * limelightScreenHeight + limelightScreenHeight/2
+                        val lowerY = boundingBoxY - limelightNetworkTable.getEntry("tvert${entryNum}").getDouble(0.0)/2
+                        val noteCoords : Vector2 = getNoteCoords(lowerY, xOffset)
+
+                        notes.add(noteCoords)
 
                     }
                 }
-
             }
         }
     }
     fun limelightToRobotCoords(position: Vector2): Vector2 {
-        return position-limelightOffset
+        return position - limelightOffset
     }
     fun seesNotes(): Boolean {
         return limelightNetworkTable.getEntry("tv").getBoolean(false)
     }
-    fun getNoteRobotCoords(boundingBoxLowerY: Int, boundingBoxX: Int): Vector2 {
+
+    fun getNoteCoords(boundingBoxLowerY : Double, xDegreeOffset: Double): Vector2 {
         val angleFromBottom = boundingBoxLowerY/limelightScreenHeight* limelightVerticalFOV
         val tangentAngle = (limelightVerticalFOV/2 - angleFromBottom) * Math.PI/180.0
-        val distance = noteDistFromCenter + 1/12 * (sin(tangentAngle) + (1- cos(tangentAngle))/tan(tangentAngle))
-        return limelightToRobotCoords(Vector2(boundingBoxX * distance, distance))
+        val distance = noteDistFromCenter + 1/12 * (sin(tangentAngle) + ((limelightHeight - (1- cos(tangentAngle)))/tan(tangentAngle)))
+        return limelightToRobotCoords(Vector2(distance/tan(xDegreeOffset), distance))
     }
 
 }
