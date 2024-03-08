29b76b18f90ef464208179c3dee8e22c7cea8ac0
TMMShared <camasinfiniteresource@gmail.com>
2024-03-07 18:24:07 -0800

    less/more static kApril

--- src/main/kotlin/org/team2471/frc2024/PoseEstimator.kt
+++ src/main/kotlin/org/team2471/frc2024/PoseEstimator.kt
@@ -33,7 +33,7 @@ object PoseEstimator {
 
 
     private var offset = Vector2(0.0, 0.0)
-    private var kAprilScalar: Double = 0.2
+    private var kAprilScalar: Double = 0.02
     var headingOffset = 0.0.degrees
     private var lastZeroTimestamp = 0.0
     val currentPose //in feet
@@ -86,7 +86,7 @@ object PoseEstimator {
             try {
 //                println("ldkhfshaldjkhfaksdhfalsdkjfhalkhfsalsdkfhalsd: ${detection.averageDistance.asFeet}")
 
-                val kAprilFinal = (kAprilScalar * (1 - detection.ambiguity) * (if (numTarget == 1) 0.025 else numTarget / 2.0)/* * cubicMap(0.0, 18.0, 1.0, 0.0, detection.averageDistance.asFeet)*/).coerceIn(0.0, 1.0)
+                val kAprilFinal = (kAprilScalar * (1 - detection.ambiguity) * (if (numTarget == 1) 0.025 else 1.0)/* * cubicMap(0.0, 18.0, 1.0, 0.0, detection.averageDistance.asFeet)*/).coerceIn(0.0, 1.0)
 //                println("wadflkajdshlkasdjhflkajdshflkasdhflkajsf ${detection.averageDistance.asFeet}")
 //                    println(detection.ambiguity)
 //                val kHeading = if (kotlin.math.abs(currentPose.y) > 15.0) kHeadingEntry.getDouble(0.001) else 0.0
